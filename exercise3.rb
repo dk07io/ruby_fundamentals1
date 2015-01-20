75bacb79b76bf933c394d8d95e28f7418d7ff3b5
Daniel Kim <dk07io@Daniels-MacBook-Pro-3.local>
2015-01-20 14:17:44 -0500

    Variables and Assignment: Exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,10 @@
+#Asks user for their name.
 puts "What is your name?"
 name=gets.chomp
 puts "Hi #{name}!"
+
+#Asks user for their age.
+puts "What is your age?"
+age=gets.chomp
+current_year=2015
+puts "You were born in #{current_year-age.to_i}"
